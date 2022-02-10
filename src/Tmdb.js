bfd882d6b524f73e764bb948ae9061daf51dddb4
eduardobazler <edubazler@gmail.com>
2022-02-10 09:35:20 -0300

    atualizando repositório

---
+++ src/Tmdb.js
@@ -0,0 +1,67 @@
+const API_KEY = '90c6813d485f14707678b8616a92d6cb';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+/* -ORiginais da Netflix
+- recomendados
+- em alta 
+-ação
+- terror
+- comédia
+- em alta
+*/
+
+const basicFeth = async (endPoint) => {
+  const req = await fetch(`${API_BASE}${endPoint}`);
+  const responseJson = await req.json();
+  return responseJson;
+}
+
+const tmdbObject = {
+  getHomeList: async () => {
+    return ([
+      {
+        slug: 'originals',
+        title: 'Originais do Netflix',
+        items: await basicFeth(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'trending',
+        title: 'Recomendados para Você',
+        items: await basicFeth(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'toprated',
+        title: 'Em alta ',
+        items: await basicFeth(`/movie/top_rated?language=pr-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'action',
+        title: 'Ação',
+        items: await basicFeth(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'comedy',
+        title: 'Comédia',
+        items: await basicFeth(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'horror',
+        title: 'Terror',
+        items: await basicFeth(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'romance',
+        title: 'Romance',
+        items: await basicFeth(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'documentary',
+        title: 'Documentário',
+        items: await basicFeth(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+      },
+    ])
+  }
+}
+
+
+export default tmdbObject
